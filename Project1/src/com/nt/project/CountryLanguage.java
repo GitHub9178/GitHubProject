a8d7332635d83c55ebc272e92f83e5084de9d775
shubhamTL <shubhamTL@gmail.com>
2018-06-10 21:19:09 +0530

    TL--Modifie In Branch1

---
+++ Project1/src/com/nt/project/CountryLanguage.java
@@ -0,0 +1,13 @@
+package com.nt.project;
+
+import java.util.Locale;
+
+public class CountryLanguage {
+	Locale l[]=null;
+ public void country(){
+   l=Locale.getAvailableLocales();
+   for(Locale locales:l) {
+	   System.out.println("country are::"+locales.getCountry());
+   }
+ }
+}
--- Project1/src/com/nt/project/HelloMessage.java
+++ Project1/src/com/nt/project/HelloMessage.java
@@ -3,9 +3,10 @@ package com.nt.project;
 public class HelloMessage {
 
 	public static void main(String[] args) {
-	
 		System.out.println("Hii");
 	    WishGenerator.wish("susanta");
+	    CountryLanguage cl=new CountryLanguage();
+	    cl.country();
 		}
 	}
 	
