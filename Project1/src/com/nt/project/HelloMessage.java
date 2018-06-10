21115da0bd4ecba0e8585ae747777afed43b7eaa
SusantaDev1 <susanDev1@gmail.com>
2018-06-10 21:38:28 +0530

    jhj

--- Project1/src/com/nt/project/HelloMessage.java
+++ Project1/src/com/nt/project/HelloMessage.java
@@ -3,10 +3,9 @@ package com.nt.project;
 public class HelloMessage {
 
 	public static void main(String[] args) {
+	
 		System.out.println("Hii");
 	    WishGenerator.wish("susanta");
-	    CountryLanguage cl=new CountryLanguage();
-	    cl.country();
 		}
 	}
 	
