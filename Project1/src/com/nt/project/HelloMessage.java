009263ca31d55defdbc908cdba9bbbc3e3015a27
laxmidhardev2 <laxmidev2@gmail.com>
2018-06-10 20:31:02 +0530

    dev2dlt

--- Project1/src/com/nt/project/HelloMessage.java
+++ Project1/src/com/nt/project/HelloMessage.java
@@ -3,7 +3,7 @@ package com.nt.project;
 public class HelloMessage {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+	
 		System.out.println("Hii");
 	    WishGenerator.wish("susanta");
 		}
